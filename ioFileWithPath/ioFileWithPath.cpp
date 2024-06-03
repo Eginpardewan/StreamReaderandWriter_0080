10cb877cdcab8667a6dea059e1e2fcb02d2f0f23
Egin Pardewan <eginpardewan@gmail.com>
2024-06-03 10:17:25 +0700

    file dengan mode menulis dan menujukan nama file yang ingin di masukan

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -1,20 +1,35 @@
-// ioFileWithPath.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <fstream>
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
-}
+int main() {
+    string baris;
+	string NamaFile;
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+	cout << "Masukan Nama File : ";
+	cin >> NamaFile;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+	//membuka file dengan mode menulis
+	ofstream outfile;
+	//menunjuk ke subuah nama file
+	outfile.open(NamaFile + ".txt", ios::out);
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true)
+	{
+		cout << "- ";
+
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
