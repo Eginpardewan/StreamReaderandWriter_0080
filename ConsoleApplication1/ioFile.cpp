e395bfedde3a8726020060b7ebfdddeddea6e9cd
Egin Pardewan <eginpardewan@gmail.com>
2024-06-03 09:54:08 +0700

    membuat variabel baris

--- ConsoleApplication1/ioFile.cpp
+++ ConsoleApplication1/ioFile.cpp
@@ -11,3 +11,22 @@ int main() {
 	ofstream outfile;
 	//menunjuk ke subuah nama file
 	outfile.open("contohfile.txt");
+
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+	//unlimited loop untuk menulis
+	while (true)
+	{
+		cout << "- ";
+
+		//mendapatkan setiap karakter dalam satu baris
+		getline(cin, baris);
+		//loop akan berhenti jika anda memasukan karakter q
+		if (baris == "q") break;
+		//menulis dan memasukan nilai dari 'baris' ke dalam file
+		outfile << baris << endl;
+	}
+
+	//selesai dalam menulis sekarang tutup filenya
+	outfile.close();
